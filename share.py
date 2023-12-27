e5abc6268d912d7327f83d4245a5b6a3a38a36ef
Ek <59480009+erwa55@users.noreply.github.com>
2023-12-27 13:24:54 -0800

    Update share.py

--- share.py
+++ share.py
@@ -1,18 +1,6 @@
 from transformers import pipeline
-
-# Load the text generation model
-pipe = pipeline("text-generation", model="mistralai/Mixtral-8x7B-v0.1")
-
-# Sample text (you can replace this with your input)
-texts = [
-    "The recent elections in the United States have led to a change in policies.",
-    "New advancements in quantum computing could revolutionize technology.",
-    "Climate change is impacting ecosystems worldwide."
-]
-
-# Generate text for each input
-for text in texts:
-    result = pipe(text, max_length=50)  # You can adjust max_length as needed
-    print(f"Input: {text}")
-    print(f"Generated Text: {result[0]['generated_text']}")
-    print("-----------")
+classifier = pipeline("zero-shot-classification", model="MoritzLaurer/DeBERTa-v3-base-mnli-fever-anli")
+sequence_to_classify = "Angela Merkel is a politician in Germany and leader of the CDU"
+candidate_labels = ["politics", "economy", "entertainment", "environment"]
+output = classifier(sequence_to_classify, candidate_labels, multi_label=False)
+print(output)
