eda6b6183aa892cfea216a4e06eb0615da77aa35
Ek <59480009+erwa55@users.noreply.github.com>
2023-12-28 10:51:51 -0800

    Update share.py

--- share.py
+++ share.py
@@ -3,4 +3,9 @@ classifier = pipeline("zero-shot-classification", model="MoritzLaurer/DeBERTa-v3
 sequence_to_classify = "Angela Merkel is a politician in Germany and leader of the CDU"
 candidate_labels = ["politics", "economy", "entertainment", "environment"]
 output = classifier(sequence_to_classify, candidate_labels, multi_label=False)
-print(output)
+
+# Pair each label with its score and find the label with the highest score
+highest_score_label = max(zip(output['labels'], output['scores']), key=lambda pair: pair[1])[0]
+
+# Print the label with the highest score
+print(highest_score_label)
