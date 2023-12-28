2a1125b0315170315066d1a8c54ae88d769a2f53
Ek <59480009+erwa55@users.noreply.github.com>
2023-12-28 10:59:58 -0800

    Update share.py

--- share.py
+++ share.py
@@ -1,7 +1,7 @@
 from transformers import pipeline
 classifier = pipeline("zero-shot-classification", model="MoritzLaurer/DeBERTa-v3-base-mnli-fever-anli")
 sequence_to_classify = "Angela Merkel is a politician in Germany and leader of the CDU"
-candidate_labels = ["politics", "economy", "entertainment", "environment"]
+candidate_labels = ["Art", "Economy", "Health", "Politics", "Science", "Society", "Sports", "Traffic", "Weather", "World"]
 output = classifier(sequence_to_classify, candidate_labels, multi_label=False)
 
 # Pair each label with its score and find the label with the highest score
